c6e5fd692a1c6be7d0e4825be2ac91e1eed27f13
Dipesh imalsina <dipeshtimalsina57@gmail.com>
2026-02-23 19:48:59 +0545

    product detail

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.static',
                 'django.template.context_processors.media',
+                'category.context_processor.menu_links',
             ],
         },
     },
